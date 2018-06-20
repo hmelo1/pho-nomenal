a72849a78e5be0a58d9b91fd1fd68631f86eb436
Penguino <heriberto@melo.nyc>
2018-06-20 10:58:03 -0400

    Store configured on redux, plotting on website view now

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index'
 
+export function configureStore(){
+  return createStore(rootReducer,
+  	compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+}
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
+export const store = configureStore()
+
+//export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
