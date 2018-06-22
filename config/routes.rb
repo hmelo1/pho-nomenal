4697b1c247b310da56c874e03fea72dd397df5ab
Penguino <heriberto@melo.nyc>
2018-06-22 18:33:51 -0400

    Cleaning up extrenuous code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   scope '/api' do
     resources :recipes
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
