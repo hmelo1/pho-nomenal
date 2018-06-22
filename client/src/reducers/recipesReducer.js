81ae514e8421f3cd600fa8375c860afa34295cd5
Penguino <heriberto@melo.nyc>
2018-06-22 15:34:56 -0400

    New reducer options for specials, appetizers, and beverages

--- client/src/reducers/recipesReducer.js
+++ client/src/reducers/recipesReducer.js
@@ -5,6 +5,12 @@ export default function recipesReducer(state = initialState.recipes, action){
   switch(action.type){
     case types.FETCH_MEALS_SUCCESS:
       return action.payload
+    case types.FETCH_APPETIZERS_SUCCESS:
+      return action.payload.filter(recipe => recipe.meal_type == "Appetizer")
+    case types.FETCH_SPECIALS_SUCCESS:
+      return action.payload.filter(recipe => recipe.meal_type == "Special")
+    case types.FETCH_BEVERAGES_SUCCESS:
+      return action.payload.filter(recipe => recipe.meal_type == "Beverage")
     default:
       return state
   }
