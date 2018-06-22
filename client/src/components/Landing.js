e7a47b8c43fe00c4e4a4b5caf8f6cfed7a5d645b
Penguino <heriberto@melo.nyc>
2018-06-22 18:35:03 -0400

    Remvoed comments from landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,34 +7,6 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import { Sidebar } from './Sidebar';
 import { Display } from './Display';
 
-/*
-export const Landing = () => {
-  return (
-    <Grid>
-      <Row>
-        <Col md={3}>
-          <Sidebar />
-        </Col>
-        <Col lg={9}>
-            <Display />
-        </Col>
-      </Row>
-    </Grid>
-  )
-}*/
-
-/*
-window.fetch('api/recipes')
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(error => console.log(error))
-}
-
-if (this.props.recipes.length == 0){
-  this.props.actions.fetchRecipes()
-}
-*/
-
 class Landing extends React.Component {
 
   render(){
@@ -54,10 +26,3 @@ class Landing extends React.Component {
 }
 
 export default Landing;
-/*
-componentDidMount() {
-  window.fetch('api/recipes')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.log(error))
-}*/
