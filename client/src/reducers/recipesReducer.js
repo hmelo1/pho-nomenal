5285dfe1dcea6a97ec1cb2aee9907d86ba1d3914
Penguino <heriberto@melo.nyc>
2018-06-22 18:41:54 -0400

    Fixed equal signs in reducer

--- client/src/reducers/recipesReducer.js
+++ client/src/reducers/recipesReducer.js
@@ -6,11 +6,11 @@ export default function recipesReducer(state = initialState.recipes, action){
     case types.FETCH_MEALS_SUCCESS:
       return action.payload
     case types.FETCH_APPETIZERS_SUCCESS:
-      return action.payload.filter(recipe => recipe.meal_type == "Appetizer")
+      return action.payload.filter(recipe => recipe.meal_type === "Appetizer")
     case types.FETCH_SPECIALS_SUCCESS:
-      return action.payload.filter(recipe => recipe.meal_type == "Special")
+      return action.payload.filter(recipe => recipe.meal_type === "Special")
     case types.FETCH_BEVERAGES_SUCCESS:
-      return action.payload.filter(recipe => recipe.meal_type == "Beverage")
+      return action.payload.filter(recipe => recipe.meal_type === "Beverage")
     default:
       return state
   }
