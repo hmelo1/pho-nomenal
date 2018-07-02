797cb0f9349ca13940b5572758fb33447b0b3b96
Penguino <heriberto@melo.nyc>
2018-07-02 16:02:26 -0400

    Review form created

--- client/src/containers/ReviewsForm.js
+++ client/src/containers/ReviewsForm.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-
-class ReviewsList extends Component {
-    render(){
-        return (
-            <div>
-              Reviews Form!
-            </div>
-        )
-    }
+const ReviewsForm = props => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <div>
+          <Field
+            name="user"
+            component="input"
+            type="text"
+            placeholder="Name"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Notes</label>
+        <div>
+          <Field name="notes" component="textarea" />
+        </div>
+      </div>
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
+    </form>
+  )
 }
 
-export default ReviewsList;
+export default reduxForm({
+  form: 'review' // a unique identifier for this form
+})(ReviewsForm)
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
-import recipes from './recipesReducer'
-import reviews from './reviewsReducer'
+import recipes from './recipesReducer';
+import reviews from './reviewsReducer';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   recipes,
-  reviews
+  reviews,
+  form: formReducer,
 });
 
 export default rootReducer;
