d12d59705bae3a90cdfbecdc258d183d64272dcd
Penguino <heriberto@melo.nyc>
2018-07-03 18:00:03 -0400

    All chang s now functioning

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,23 @@
 class ReviewsController < ApiController
-    def index
+  def index
+    @reviews = Review.all
+    render json: {reviews: @reviews.as_json}
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
       @reviews = Review.all
       render json: {reviews: @reviews.as_json}
-    end
-  
-    def show
-      @recipe = Recipe.find(params[:id])
-      render json: @recipes.to_json
+    else 
+      render json: { error: "Error creating post!s"}
     end
   end
+
+  private
+
+  def review_params
+    params.permit(:user, :review)
+  end  
+end
   
